88cd6c69e9b732c40dc8cacf79bda6790cf8f214
Jose Benjamin Valencia Rivera <benjarq.89@gmail.com>
2026-05-01 17:13:09 -0600

    mi primer commit: Clase de Libros.java lista

---
+++ src/Libros.java
@@ -0,0 +1,23 @@
+public class Libro extends Material {
+    private String autor;
+    private int numPaginas;
+    private String editorial;
+    private String isbn;
+    private int anioPublicacion;
+
+    public Libro(String codigoId, String titulo, String ubicacionFisica, int ejemplaresTotales,
+                 String autor, int numPaginas, String editorial, String isbn, int anioPublicacion) {
+        // 'super' llama al constructor del padre (Material)
+        super(codigoId, titulo, ubicacionFisica, ejemplaresTotales);
+        this.autor = autor;
+        this.numPaginas = numPaginas;
+        this.editorial = editorial;
+        this.isbn = isbn;
+        this.anioPublicacion = anioPublicacion;
+    }
+
+    @Override
+    public void mostrarDetalles() {
+        System.out.println("ID: " + codigoId + " | Título: " + titulo + " | Autor: " + autor);
+    }
+}
